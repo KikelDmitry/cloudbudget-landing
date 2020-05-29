8b4eee6e3ea246cab8e12b0fec131d330956dc38
Kikel Dmitry <kikeldmitry@gmail.com>
2020-05-30 01:41:28 +0300

    almost adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,30 @@ window.addEventListener('DOMContentLoaded', function () {
 		svg4everybody();
 	}
 })
+
+let burgerBtn = document.querySelector('.top-burger__btn');
+
+burgerBtn.addEventListener('click', mobileMenu)
+
+function mobileMenu() {
+	if (!this.classList.contains('is-active')) {
+		this.classList.add('is-active');
+	} else {
+		this.classList.remove('is-active');
+	}
+}
+window.addEventListener('click', function(e) {
+	if(e.target.tagName != 'BUTTON') {
+		return;		
+	} else {
+		console.log('button click!')
+	}
+})
+
+//fake slider
+let slider = document.querySelector('.slider-cont');
+function fakeSlider() {
+	let slide = slider.querySelector('.slider-cont__item');
+	slider.append(slide);
+}
+slider.onclick = fakeSlider;
